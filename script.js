bf6fef5aef8ba6f6ada519254f2a38d47c27eeb7
IWPW03 <putrawidnyana70@gmail.com>
2026-08-11 14:41:32 +0800

    Update API_URL to new Google Apps Script URL

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 
 /* URL Google Apps Script yang menjadi sumber data.
    Ganti nilai konstanta ini dengan URL Web App Anda. */
-const API_URL = "https://script.google.com/macros/s/AKfycbzKFLnMqtdO3XGDjZRgMOSPMqhmzojIHvv9_HN2wwoDGvndnRqRFREOlE1UraTKwWzO4A/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzc04bZ7ilAOlxCASJBczwNAw3DBVjutv3qB4I4EbDyIZ45eE8Qg2HHIaIDOtO7PEwp3A/exec";
 
 /* Penanganan error agar aplikasi tidak crash saat API bermasalah */
 const SafeState = {
